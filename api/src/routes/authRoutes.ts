4621c55c1935d07ed6696d1567b8eb410af5acab
Jun <hanjun.kim.dev@gmail.com>
2024-06-29 10:32:42 +0200

    fix

--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -3,14 +3,13 @@ import { signup } from '../controllers/auth/signup';
 import { login } from '../controllers/auth/login';
 import { logout } from '../controllers/auth/logout';
 import { detail } from '../controllers/auth/detail';
-import jwtBlacklist from '../middlewares/jwtBlacklist';
 import authMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/logout', jwtBlacklist, logout);
+router.post('/logout', authMiddleware, logout);
 router.get('/details', authMiddleware, detail);
 
 export default router;
