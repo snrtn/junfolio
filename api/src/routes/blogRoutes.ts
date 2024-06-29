2e3fb84e0d3db42438eadaaabef18f663bac7a15
Jun <hanjun.kim.dev@gmail.com>
2024-06-29 10:29:20 +0200

    fix

--- api/src/routes/blogRoutes.ts
+++ api/src/routes/blogRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authMiddleware from '../middlewares/authMiddleware';
 import { createPost } from '../controllers/blog/createPost';
 import { updatePost } from '../controllers/blog/updatePost';
 import { deletePost } from '../controllers/blog/deletePost';
@@ -9,8 +10,8 @@ const router = Router();
 
 router.get('/', getPosts);
 router.get('/:id', getPostById);
-router.post('/', createPost);
-router.put('/:id', updatePost);
-router.delete('/:id', deletePost);
+router.post('/', authMiddleware, createPost);
+router.put('/:id', authMiddleware, updatePost);
+router.delete('/:id', authMiddleware, deletePost);
 
 export default router;
